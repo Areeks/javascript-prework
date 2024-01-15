64e2bdb19fc29fd9f934cc6d45d3ac80b23ef11b
Arkadiusz Kazmierczak <areeeks@gmail.com>
2024-01-15 22:35:46 +0100

    Added button to index.html and add functionality script at the click of buttons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let computerMove = randomMove();
-console.log('Gracz wpisał: ' + playerInput);
+function playGame(playerInput){
+    clearMessages();
+    let computerMove = randomMove();
+    console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(playerInput);
+    let playerMove = getMoveName(playerInput);
 
-displayResult(getMoveName(computerMove),getMoveName(playerInput));
+    displayResult(getMoveName(computerMove),getMoveName(playerInput));
 
-printMessage('Twój ruch to: ' + playerMove);
+    printMessage('Twój ruch to: ' + playerMove);
+}
+
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
+  document.getElementById('play-peper').addEventListener('click', function(){
+    playGame(2);
+  });
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
+
+
+
+
